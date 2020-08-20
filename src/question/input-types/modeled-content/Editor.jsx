c3e6c1c8245b7fb685a0554cffb113a1f951ea53
Andrew Ligon <andrew.ligon@nextthought.com>
2020-08-19 19:49:23 -0500

    stub out the modeled content input editor

--- src/question/input-types/modeled-content/Editor.jsx
+++ src/question/input-types/modeled-content/Editor.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import classnames from 'classnames/bind';
+import {Text} from '@nti/web-commons';
+import {scoped} from '@nti/lib-locale';
+
+import Styles from './Styles.css';
+
+const cx = classnames.bind(Styles);
+const t = scoped('nti-assessment.question.input-types.modeled-content.Editor', {
+	placeholder: 'Students will write here...'
+});
 
 export default function ModeledContentEditor () {
 	return (
-		<div>
-			Modeled Content Editor
+		<div className={cx('modeled-content-editor')}>
+			<Text.Base className={cx('placeholder')}>{t('placeholder')}</Text.Base>
 		</div>
 	);
 }
