04ccc44d7462144a026b2432c0539697cc449211
Andrew Ligon <andrew.ligon@nextthought.com>
2020-09-07 20:35:42 -0500

    add the feature check classes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '@nti/style-common/variables.css';
 import '@nti/style-common/all.scss';
+import {addFeatureCheckClasses} from '@nti/lib-dom';
+
+addFeatureCheckClasses();
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
