73c7f76bff343259b496ce732ca5b03b7eb1f16e
Andrew Ligon <andrew.ligon@nextthought.com>
2020-11-20 17:31:48 -0600

    start fleshing out the screenshot tests

--- src/__test__/Storyshots.spec.js
+++ src/__test__/Storyshots.spec.js
@@ -2,21 +2,45 @@
 import path from 'path';
 
 import initStoryshots from '@storybook/addon-storyshots';
-import { puppeteerTest } from '@storybook/addon-storyshots-puppeteer';
+import {puppeteerTest} from '@storybook/addon-storyshots-puppeteer';
 import '@wordpress/jest-puppeteer-axe';
-import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import {toMatchImageSnapshot} from 'jest-image-snapshot';
 
 expect.extend({ toMatchImageSnapshot });
 
+const BaseA11yOptions = {};
+
+const BaseScreenShotOptions = {};
+const BaseScreenShotMatchOptions = {};
+
 async function a11yCheck (page, test, options) {
-	const params = test?.a11y ?? {};
-	const include = test?.element ?? '#root';
+	if (test?.disableA11y) { return; }
+
+	const params = {
+		...BaseA11yOptions,
+		...(options.context.parameters?.a11y ?? {}),
+		...(test?.a11y ?? {})
+	};
+	const include = params?.element ?? '#root';
 
 	await expect(page).toPassAxeTests({...params, include});
 }
 
-function snapshotCheck (page, options) {
+function snapshotCheck (page, test, options) {
+	if (test?.disableScreenshot) { return; }
 
+	const {screenshot} = test ?? {};
+
+	const {before, getTarget, after, options, matchOptions} = test ?? {};
+
+	await before?.(page);
+
+	const element = await getTarget?.(page);
+	const image = await (element ?? page).screenshot({...BaseScreenShotOptions, ...(options ?? {})});
+
+	await after?.(image, page);
+
+	expect(image).toMatchImageSnapshot({...BaseScreenShotMatchOptions, ...(matchOptions ?? {})});
 }
 
 const runner = puppeteerTest({
